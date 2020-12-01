cba04a80f6640bf06fde3287e6df0a620a954005
Jason Moses <61612668+SwiftyPython@users.noreply.github.com>
2020-12-01 14:08:58 +0200

    Add files via upload

---
+++ AdamSendsMesages.py
@@ -0,0 +1,42 @@
+//Tip run on wayscript to send WhatsApp messages everyday
+import os
+from twilio.rest import Client
+import datetime
+data = datetime.datetime.today()
+datetime.datetime(data.year,data.month,data.day,data.hour,data.minute)
+index = datetime.datetime.today().weekday()
+ACCOUNT_SID = "ACa44bf46ea3b7ea23607e30c830f52449"
+AUTH_TOKEN = "e1f7670ab37e57b3911a153f634479cb"
+client = Client(ACCOUNT_SID,AUTH_TOKEN)
+day_name= ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday','Monday']
+print(day_name[index])
+print(index)
+if day_name[index] == 'Sunday':
+  client.messages.create( body="Morning sir (JARVIS)" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+if day_name[index] == 'Monday':
+  client.messages.create( body= "Morning sir (JARVIS) , STK P1 8:00 - 8:50 , OBS L1 9:00 - 9:50 , BAM L1 11:00 - 11:50 , FRK 14:00 - 15:50 ,  BAM P1 16:00 - 16:50",
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+if day_name[index] == 'Tuesday':
+  client.messages.create( body="Morning sir (JARVIS) , STK P1 8:00 - 8:50 , FRK T1 10:00 - 11:50 , BAM L2 12:00 - 12:50 , LST 13:00 - 14:50 ,  BAM P1 16:00 - 16:50" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+
+if day_name[index] == 'Wednesday':
+  client.messages.create( body="Morning sir (JARVIS) , FRK T2 8:00 - 8:50 , STK T1 9:00 - 9:50 , STK L2 13:00 - 13:50 , OBS L2 14:00 - 14:50" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+if day_name[index] == 'Thursday':
+  client.messages.create( body="Morning sir (JARVIS) , FRK L2 8:00 - 9:50 , LST L2 11:00 - 12:50 , AIM P1 13:00 - 14:50 , OBS T1 15:00 - 16:50" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+if day_name[index] == 'Friday':
+  client.messages.create( body="Morning sir (JARVIS) ,BAM L3 13:00 - 13:50" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
+if day_name[index] == 'Saturday':
+  client.messages.create( body="Morning sir (JARVIS)" ,
+                         to="whatsapp:+27762621248",
+                from_="whatsapp:+14155238886")
